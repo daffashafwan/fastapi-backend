939442cf380b31d5f22e1cd9bf5bca7fe5c412f9
Daffa Shafwan <moch.chairullah@tiket.com>
2023-11-21 17:27:42 +0700

    init commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import mysql.connector
+
+app = FastAPI()
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+# Connect to MySQL database
+db = mysql.connector.connect(
+    host="localhost",
+    port="5432",
+    user="daffashafwan",
+    passwd="",
+    database="appdb"
+)
+
+@app.post("/login")
+async def login(user: UserLogin):
+    cursor = db.cursor()
+
+    # Check if the user exists in the database
+    query = "SELECT * FROM users WHERE username = %s AND password = %s"
+    cursor.execute(query, (user.username, user.password))
+    result = cursor.fetchone()
+
+    if result:
+        return {"message": "Login successful"}
+    else:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
